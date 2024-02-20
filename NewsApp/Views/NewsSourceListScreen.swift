f0044a3f82ae87c7b2d8719b444520b1d1d6461f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-02-21 00:01:42 +0900

    perform asynchronous action from synchronous code

--- NewsApp/Views/NewsSourceListScreen.swift
+++ NewsApp/Views/NewsSourceListScreen.swift
@@ -23,7 +23,9 @@ struct NewsSourceListScreen: View {
             }
             .navigationTitle("News Sources")
             .navigationBarItems(trailing: Button(action: {
-                // refresh the news
+                Task {
+                    await newsSourceListViewModel.getSources()
+                }
             }, label: {
                 Image(systemName: "arrow.clockwise.circle")
             }))
